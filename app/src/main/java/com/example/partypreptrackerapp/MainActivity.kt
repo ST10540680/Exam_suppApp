985430ad86025b806c895d8e397b82c4c1ee4e44
ST10540680 <ST10540680@myemeris.edu.za>
2026-07-28 09:56:42 +0200

    called the installSplashScreen before setting content view

--- app/src/main/java/com/example/partypreptrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/partypreptrackerapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.partypreptrackerapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // installing the splash screen before setting content view
+        val splashScreen =installSplashScreen()
+        
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
